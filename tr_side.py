dc18c2d7968c2b314b2d0e169836937db58799ca
Adern <magmed145@gmail.com>
2025-02-28 23:34:15 +0200

    Реализовал поиск первого катета в  прямоугольном триугольника

--- tr_side.py
+++ tr_side.py
@@ -66,6 +66,9 @@ def tr_side(main_win):
         entry_AB = Entry(width=5, validate='key', validatecommand=vcmd)
         entry_AB.place(anchor=CENTER, x=350, y=230)
 
+        btn_back= Button(text="В начало", width=8, height=2, compound="top",  font=("Arial", 16), command=lambda: tr_side(main_win))
+        btn_back.place(anchor="c", y=130, x = 1000)
+
         def start():
             if entry_AB.get() == '' or entry_AC.get() == '':
                 error_str.set("Необходимо ввести значение")
@@ -81,6 +84,23 @@ def tr_side(main_win):
                 AB = float(entry_AB.get())
                 AC = float(entry_AC.get())
             
+                entry_AB.destroy()
+                entry_AC.destroy()
+                btn_start.destroy()
+
+                lbl_AC = Label(text=AC,font=["Arial", 14], fg="white", bg="black")
+                lbl_AC.place(anchor=CENTER, x=140, y=250)
+
+                lbl_AB = Label(text=AB,font=["Arial", 14], fg="white", bg="black")
+                lbl_AB.place(anchor=CENTER, x=350, y=230)
+
+                BC = round(math.sqrt(pow(AB, 2)-pow(AC, 2)),2)
+                lbl_BC = Label(text=BC,font=["Arial", 14], fg="white", bg="black")
+                lbl_BC.place(anchor=CENTER, x=280, y=400)
+
+                lbl_teor = Label(justify=LEFT, font=["Arial", 22],bg="black", fg="White", text=f"BC = √(AB²-AC²)\nBC = √({AB}²-{AC}²)\nBC = √({pow(AB,2)}-{pow(AC,2)})\nBC = √{pow(AB,2)-pow(AC,2)}\nBC = {BC}")
+                lbl_teor.place( anchor=CENTER,x=700, y=200)
+
         btn_start = Button(text="Знайти сторону",font=["Arial",14], width=15, height=2, command=start)
         btn_start.place(anchor=CENTER, x=285, y=470)
 
@@ -153,7 +173,7 @@ def tr_side(main_win):
     btn_AC = Button(text="AC", width=5, height=2, command=search_AC)
     btn_AC.place(anchor=CENTER, x=140, y=250)
 
-    btn_BC = Button(text="CB", width=5, height=2, command=search_BC)
+    btn_BC = Button(text="BC", width=5, height=2, command=search_BC)
     btn_BC.place(anchor=CENTER, x=280, y=400)
 
     btn_AB = Button(text="AB", width=5, height=2, command=search_AB)
