a6fd56604be869f024dfa0d4c2d0dd8a7b832158
Adern <magmed145@gmail.com>
2025-03-02 22:44:49 +0200

    Исправил ошибку в таблице углов

--- alg_search/corners_table.py
+++ alg_search/corners_table.py
@@ -28,31 +28,36 @@ def corners_table(main_win):
     lbl_four = Label(text="",justify=LEFT, bg="black", fg="white", font=["Arial", 14])
     lbl_four.place(anchor=NW,  x=800, y=40)
 
+    error_str = StringVar()
+    lbl_error = Label(textvariable=error_str, font=["Arial", 14], fg="red", bg="black")
+    lbl_error.place(anchor=CENTER, x=540, y=400)
+
     def page_list(action: str, value_ent: Entry = 1):
         global page
         my_page = ["","","",""]
+        error_str.set("")
         if(action == "start"):
             for val in range((page-1)*values , page*values+1):
-                my_page[0] = my_page[0] + f"sin({val}) = {round(math.sin(val),2)}\n"
-                my_page[1] = my_page[1] + f"cos({val}) = {round(math.cos(val),2)}\n"
-                my_page[2] = my_page[2] + f"tan({val}) = {round(math.tan(val),2)}\n"
+                my_page[0] = my_page[0] + f"sin({val}) = {round(math.sin(math.radians(val)),2)}\n"
+                my_page[1] = my_page[1] + f"cos({val}) = {round(math.cos(math.radians(val)),2)}\n"
+                my_page[2] = my_page[2] + f"tan({val}) = {round(math.tan(math.radians(val)),2)}\n"
                 if math.sin(val) == 0:
                     my_page[3] = my_page[3] + f"ctg({val}) = - \n"
                 else:
-                    my_page[3] = my_page[3] + f"ctg({val}) = {round(math.cos(val)/math.sin(val), 2)}\n"
+                    my_page[3] = my_page[3] + f"ctg({val}) = {round(math.cos(math.radians(val))/math.sin(math.radians(val)), 2)}\n"
                 
 
         elif(action == "right"):
             page = page + 1
             for val in range((page-1)*values , page*values+1):
                 val = val +1
-                my_page[0] = my_page[0] + f"sin({val}) = {round(math.sin(val),2)}\n"
-                my_page[1] = my_page[1] + f"cos({val}) = {round(math.cos(val),2)}\n"
-                my_page[2] = my_page[2] + f"tan({val}) = {round(math.tan(val),2)}\n"
+                my_page[0] = my_page[0] + f"sin({val}) = {round(math.sin(math.radians(val)),2)}\n"
+                my_page[1] = my_page[1] + f"cos({val}) = {round(math.cos(math.radians(val)),2)}\n"
+                my_page[2] = my_page[2] + f"tan({val}) = {round(math.tan(math.radians(val)),2)}\n"
                 if math.sin(val) == 0:
                     my_page[3] = my_page[3] + f"ctg({val}) = - \n"
                 else:
-                    my_page[3] = my_page[3] + f"ctg({val}) = {round(math.cos(val)/math.sin(val), 2)}\n"
+                    my_page[3] = my_page[3] + f"ctg({val}) = {round(math.cos(math.radians(val))/math.sin(math.radians(val)), 2)}\n"
 
 
         elif(action == "left"):
@@ -63,24 +68,20 @@ def corners_table(main_win):
             for val in range((page-1)*values , page*values+1):
                 if page != 1:
                     val = val + 1
-                my_page[0] = my_page[0] + f"sin({val}) = {round(math.sin(val),2)}\n"
-                my_page[1] = my_page[1] + f"cos({val}) = {round(math.cos(val),2)}\n"
-                my_page[2] = my_page[2] + f"tan({val}) = {round(math.tan(val),2)}\n"
+                my_page[0] = my_page[0] + f"sin({val}) = {round(math.sin(math.radians(val)),2)}\n"
+                my_page[1] = my_page[1] + f"cos({val}) = {round(math.cos(math.radians(val)),2)}\n"
+                my_page[2] = my_page[2] + f"tan({val}) = {round(math.tan(math.radians(val)),2)}\n"
                 if math.sin(val) == 0:
                     my_page[3] = my_page[3] + f"ctg({val}) = - \n"
                 else:
-                    my_page[3] = my_page[3] + f"ctg({val}) = {round(math.cos(val)/math.sin(val), 2)}\n"
+                    my_page[3] = my_page[3] + f"ctg({val}) = {round(math.cos(math.radians(val))/math.sin(math.radians(val)), 2)}\n"
 
 
         elif(action == "search"):
             value = int(value_ent.get())
-            if(value<1):
-                error_str.set("Значение должно быть > 0")
-                return
-            elif(value>1000):
+            if(value>1000):
                 error_str.set("Значение должно быть < 1001")
                 return
-        
             while True:
                 if (page-1)*values < value and values+(page-1)*values < value:
                     page = page + 1
@@ -92,13 +93,13 @@ def corners_table(main_win):
             for val in range((page-1)*values , page*values+1):
                 if page != 1:
                     val = val + 1
-                my_page[0] = my_page[0] + f"sin({val}) = {round(math.sin(val),2)}\n"
-                my_page[1] = my_page[1] + f"cos({val}) = {round(math.cos(val),2)}\n"
-                my_page[2] = my_page[2] + f"tan({val}) = {round(math.tan(val),2)}\n"
+                my_page[0] = my_page[0] + f"sin({val}) = {round(math.sin(math.radians(val)),2)}\n"
+                my_page[1] = my_page[1] + f"cos({val}) = {round(math.cos(math.radians(val)),2)}\n"
+                my_page[2] = my_page[2] + f"tan({val}) = {round(math.tan(math.radians(val)),2)}\n"
                 if math.sin(val) == 0:
                     my_page[3] = my_page[3] + f"ctg({val}) = - \n"
                 else:
-                    my_page[3] = my_page[3] + f"ctg({val}) = {round(math.cos(val)/math.sin(val), 2)}\n"
+                    my_page[3] = my_page[3] + f"ctg({val}) = {round(math.cos(math.radians(val))/math.sin(math.radians(val)), 2)}\n"
 
 
         lbl_one["text"] = my_page[0]
@@ -120,10 +121,6 @@ def corners_table(main_win):
 
     btn_right = Button(text=">", width=2, height=1, compound="top",  font=("Arial", 16), command=lambda: page_list("right"))
     btn_right.place(anchor=CENTER, x=680, y=500)
-
-    error_str = StringVar()
-    lbl_error = Label(textvariable=error_str, font=["Arial", 14], fg="red", bg="black")
-    lbl_error.place(anchor=CENTER, x=540, y=400)
 
     btn_back= Button(text="В меню", width=8, height=2, compound="top",  font=("Arial", 16), command=lambda: MainWin.MainWin(main_win))
     btn_back.place(anchor="c", y=500, x = 1000)
